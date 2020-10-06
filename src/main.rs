c67c7f988de6b22295fadc195722032fd260d7b7
CyberWolf <borel.loic.37@gmail.com>
2020-10-06 21:40:41 +0200

    add input system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod pong;
+mod systems;
 
 use crate::pong::Pong;
 
@@ -11,7 +12,8 @@ use amethyst::{
     },
     utils::application_root_dir,
 };
-
+use amethyst::input::{InputBundle, StringBindings};
+use amethyst::core::TransformBundle;
 
 
 fn main() -> amethyst::Result<()> {
@@ -21,8 +23,14 @@ fn main() -> amethyst::Result<()> {
     // Set config
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
 
     let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]) 
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
@@ -33,6 +41,7 @@ fn main() -> amethyst::Result<()> {
                 // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
                 .with_plugin(RenderFlat2D::default()),
         )?;
+        
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
